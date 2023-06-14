fcf313761ae37a728b862f795c2d29a26299191e
AleksKar <Alekseykar92@gmail.com>
2023-06-14 19:03:20 +0100

    feat/s-2: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
 import './index.scss';
 import 'macro-css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './App';
 
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
